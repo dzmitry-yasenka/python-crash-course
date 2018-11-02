8b1c38d2c8ff94d00453de46a107eaeace206d24
Dzmitry Yasenka <dzmitry.yasenka@gmail.com>
2018-11-02 23:21:34 +0000

    Chapter 2

---
+++ chapter-2/chapter-2.py
@@ -0,0 +1,20 @@
+message = "Hello Python world!"
+print(message)
+
+message = "Hello me!"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+print("Hello, " + full_name.title() + "!")
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
